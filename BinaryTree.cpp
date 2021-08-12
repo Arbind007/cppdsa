5f8e6466a94c6d9f235f39d750d6dd802cda6cd7
Arbind007 <almbbsr456@gmail.com>
2021-08-12 13:30:42 +0530

    sortest dis between node

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -388,6 +388,65 @@ void leftView(Node *root)
         }
     }
 }
+
+Node *LCA(Node *root, int n1, int n2)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->data == n1 || root->data == n2)
+    {
+        return root;
+    }
+    Node *left = LCA(root->left, n1, n2);
+    Node *right = LCA(root->right, n1, n2);
+
+    if (left != NULL && right != NULL)
+    { // both left and right have either n1 or n2
+        return root;
+    }
+
+    if (right == NULL && left == NULL)
+    { //not found
+        return NULL;
+    }
+    if (left != NULL)
+    {
+        return LCA(root->left, n1, n2);
+    }
+    return LCA(root->right, n1, n2);
+}
+
+int findDis(Node *root, int k, int dist)
+{
+    if (root == NULL)
+    {
+        return -1;
+    }
+    if (root->data == k)
+    {
+        return dist;
+    }
+
+    int left = findDis(root->left, k, dist + 1);
+    if (left != -1)
+    {
+        return left;
+    }
+    return findDis(root->right, k, dist + 1);
+}
+
+int disBtwNodes(Node *root, int n1, int n2)
+{
+    Node *lca = LCA(root, n1, n2);
+
+    int d1 = findDis(lca, n1, 0);
+    int d2 = findDis(lca, n2, 0);
+
+    return d1 + d2;
+}
+
 int main()
 {
     struct Node *root = new Node(1);
@@ -395,8 +454,8 @@ int main()
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
+    // root->right->left = new Node(6);
+    // root->right->right = new Node(7);
     // preorder(root);
     // inorder(root);
     // postorder(root);
@@ -423,6 +482,8 @@ int main()
     // int height = 0;
     // cout << isbalance2(root, &height) << endl;
 
-    leftView(root);
+    // leftView(root);
+
+    cout << disBtwNodes(root, 4, 5) << endl;
     return 0;
 }
