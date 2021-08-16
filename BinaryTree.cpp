bf12924d748e5473dd16b83d25fdcf4c88c83e5c
Arbind007 <almbbsr456@gmail.com>
2021-08-16 11:15:02 +0530

    Nodes at Distance K in Binary Tree

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -471,14 +471,69 @@ void flatten(Node *root)
     flatten(root->right);
 }
 
+void printSubtreeNodes(Node *root, int k)
+{
+    if (root == NULL || k < 0)
+    {
+        return;
+    }
+    if (k == 0)
+    {
+        cout << root->data << ' ';
+    }
+    printSubtreeNodes(root->left, k - 1);
+    printSubtreeNodes(root->right, k - 1);
+}
+
+int printNodeatK(Node *root, Node *target, int k)
+{
+    if (root == NULL)
+    {
+        return -1;
+    }
+    if (root == target)
+    {
+        printSubtreeNodes(root, k);
+        return 0;
+    }
+    int dl = printNodeatK(root->left, target, k);
+    if (dl != -1)
+    {
+        if (dl + 1 == k)
+        {
+            cout << root->data << ' ';
+        }
+        else
+        {
+            printSubtreeNodes(root->right, k - dl - 2);
+        }
+        return 1 + dl;
+    }
+    int dr = printNodeatK(root->right, target, k);
+    if (dr != -1)
+    {
+        if (dr + 1 == k)
+        {
+            cout << root->data << ' ';
+        }
+        else
+        {
+            printSubtreeNodes(root->left, k - dr - 2);
+        }
+        return 1 + dr;
+    }
+
+    return -1;
+}
+
 int main()
 {
-    struct Node *root = new Node(4);
-    root->left = new Node(9);
-    root->right = new Node(5);
-    root->left->left = new Node(1);
-    root->left->right = new Node(3);
-    root->right->right = new Node(6);
+    // struct Node *root = new Node(4);
+    // root->left = new Node(9);
+    // root->right = new Node(5);
+    // root->left->left = new Node(1);
+    // root->left->right = new Node(3);
+    // root->right->right = new Node(6);
     // root->right->left = new Node(6);
     // root->right->right = new Node(7);
     // preorder(root);
@@ -511,8 +566,13 @@ int main()
 
     // cout << disBtwNodes(root, 4, 5) << endl;
 
-    flatten(root);
-    printinorder(root);
+    // flatten(root);
+    // printinorder(root);
 
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    printNodeatK(root, root->left, 1);
     return 0;
 }
