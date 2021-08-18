28ca4b12206f3f292deef3b6cc6bb8e1fad7e2e4
Arbind007 <almbbsr456@gmail.com>
2021-08-18 18:43:01 +0530

    lca

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -526,16 +526,59 @@ int printNodeatK(Node *root, Node *target, int k)
     return -1;
 }
 
+bool getPath(Node *root, int key, vector<int> &path)
+{
+
+    if (root == NULL)
+    {
+        return false;
+    }
+    path.push_back(root->data);
+    if (root->data == key)
+    {
+        return true;
+    }
+
+    if (getPath(root->left, key, path) || getPath(root->right, key, path))
+    {
+        return true;
+    }
+
+    path.pop_back();
+
+    return false;
+}
+
+int LCA2(Node *root, int n1, int n2)
+{
+    vector<int> path1, path2;
+    if (!getPath(root, n1, path1) || !getPath(root, n2, path2))
+    {
+        return -1;
+    }
+
+    int pc;
+    for (pc = 0; pc < path1.size() && path2.size(); pc++)
+    {
+        if (path1[pc] != path2[pc])
+        {
+            break;
+        }
+    }
+
+    return path1[pc - 1];
+}
+
 int main()
 {
-    // struct Node *root = new Node(4);
-    // root->left = new Node(9);
-    // root->right = new Node(5);
-    // root->left->left = new Node(1);
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
     // root->left->right = new Node(3);
-    // root->right->right = new Node(6);
-    // root->right->left = new Node(6);
-    // root->right->right = new Node(7);
+    root->right->right = new Node(6);
+    root->right->left = new Node(5);
+    root->right->left->left = new Node(7);
     // preorder(root);
     // inorder(root);
     // postorder(root);
@@ -569,10 +612,24 @@ int main()
     // flatten(root);
     // printinorder(root);
 
-    Node *root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    printNodeatK(root, root->left, 1);
+    // Node *root = new Node(1);
+    // root->left = new Node(2);
+    // root->right = new Node(3);
+    // root->left->left = new Node(4);
+    // printNodeatK(root, root->left, 1);
+
+    int n1 = 7;
+    int n2 = 6;
+    Node *lca = LCA(root, n1, n2);
+
+    if (lca == NULL)
+    {
+        cout << "LCA does not exist" << endl;
+    }
+    else
+    {
+        cout << "LCA : " << lca->data << endl;
+    }
+
     return 0;
 }
